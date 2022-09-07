8fcf8767c9eaf23b222c12c96049ebf3f92285ed
{The BOSS} David Parra-Sandoval  A MAN!!!!!! <Parralogic@gmail.com>
2022-09-07 10:56:09 -0700

    Update NASA.sh

--- NASA.sh
+++ NASA.sh
@@ -4,7 +4,10 @@
 #Last Modified: 09/07/2022
 clear
 
+FLAT () {
 while true; do
+local ARRAY=( 'SPACE' 'satellite' ' ' )
+local NASA=${ARRAY[0]}
 if [[ $NASA = "satellite" ]]; then
 echo NASA is Fabricated BullShit!
 elif [[ $NASA = "SPACE" ]]; then
@@ -12,3 +15,5 @@ echo For Real?
 else echo NASA thinks they going to Mars with a leaky fuel tank.
 fi
 done
+}
+FLAT
