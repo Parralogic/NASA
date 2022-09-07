d6df57db51c5ebd19020b2d41b0fe51ddfd62d43
{The BOSS} David Parra-Sandoval  A MAN!!!!!! <Parralogic@gmail.com>
2022-09-07 10:36:56 -0700

    Update NASA.sh

--- NASA.sh
+++ NASA.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Creator: David Parra-Sandoval
 #Date: 09/06/2022
-#Last Modified: 09/06/2022
+#Last Modified: 09/07/2022
 clear
 
 while true; do
-if [[ NASA = "satellite" ]]; then
+if [[ $NASA = "satellite" ]]; then
 echo NASA is Fabricated BullShit!
-elif [[ NASA = "SPACE" ]]; then
+elif [[ $NASA = "SPACE" ]]; then
 echo For Real?
 else echo NASA thinks they going to Mars with a leaky fuel tank.
 fi
